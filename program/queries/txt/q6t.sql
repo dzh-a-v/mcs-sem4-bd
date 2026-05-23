2f7a8dc645a4676d783eb54d9c338ba1c4f98736
dzh-a-v <metroexoduslover@gmail.com>
2026-05-23 13:58:15 +0300

    fix: removed inner select in q6

--- program/queries/txt/q6t.sql
+++ program/queries/txt/q6t.sql
@@ -1,16 +1,31 @@
-SELECT g.galaxy_id
+SELECT DISTINCT g.galaxy_id
 FROM galaxy g
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM cluster c
-    JOIN stellar_system s ON s.cluster_id = c.cluster_id
-    JOIN plan_system ps ON ps.stell_sys_id = s.stellar_id
-    JOIN exoplanet e ON e.sys_id = ps.system_id
-    JOIN observation o ON o.exo_id = e.exo_id
-    JOIN telescope t ON t.tele_id = o.tele_id
-    WHERE c.galaxy_id = g.galaxy_id
-      AND t.tele_type = 'ground'
-);
+JOIN cluster c          ON c.galaxy_id   = g.galaxy_id
+JOIN stellar_system ss  ON ss.cluster_id = c.cluster_id
+JOIN plan_system ps     ON ps.stell_sys_id = ss.stellar_id
+LEFT JOIN exoplanet e   ON e.sys_id = ps.system_id
+LEFT JOIN observation o ON o.exo_id  = e.exo_id
+LEFT JOIN telescope t   ON t.tele_id = o.tele_id
+                       AND t.tele_type = 'ground'
+GROUP BY g.galaxy_id
+HAVING COUNT(t.tele_id) = 0;
+
+
+
+
+-- SELECT g.galaxy_id
+-- FROM galaxy g
+-- WHERE NOT EXISTS (
+--     SELECT 1
+--     FROM cluster c
+--     JOIN stellar_system s ON s.cluster_id = c.cluster_id
+--     JOIN plan_system ps ON ps.stell_sys_id = s.stellar_id
+--     JOIN exoplanet e ON e.sys_id = ps.system_id
+--     JOIN observation o ON o.exo_id = e.exo_id
+--     JOIN telescope t ON t.tele_id = o.tele_id
+--     WHERE c.galaxy_id = g.galaxy_id
+--       AND t.tele_type = 'ground'
+-- );
 
 
 
